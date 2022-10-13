23961760cc2b65431519a63dc9675f1f00f1fae9
Henrique Buss <henrique.buss@hotmail.com>
2022-10-12 23:22:47 -0300

    Fix

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,7 +14,9 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/ignite-lab-design-system";
+      config.base = "/ignite-lab-design-system/";
     }
+
+    return config;
   },
 };
